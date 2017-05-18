19d54593ddb851edbfb54eea7bda127f0dc0ac1a
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-05-18 08:32:23 +0300

    db/ldb

---
+++ db/ldb/DB.go
@@ -0,0 +1,207 @@
+package db
+
+
+// @author  Mikhail Kirillov
+// @email   mikkirillov@yandex.ru
+// @version 1.000
+// @date    2017-05-18
+
+
+import (
+    "github.com/syndtr/goleveldb/leveldb"
+    "github.com/syndtr/goleveldb/leveldb/util"
+    "sync"
+)
+
+
+type DB struct {
+    sync.Mutex
+    dbpath string
+    ldb *leveldb.DB
+}
+
+
+type FOR_EACH_KEY_FUNC func([]byte) bool
+
+
+var _db *DB
+
+
+func Init( base string ) {
+    if _db == nil {
+        _db = &DB{
+            dbpath: base,
+        }
+
+        _db.ldb,_ = leveldb.OpenFile(base,nil)
+    
+    }
+}
+
+
+func SetUnsafe( key []byte, value []byte) {
+    if value == nil || len(value) == 0 {
+        _db.ldb.Delete( key, nil )
+    } else {
+        _db.ldb.Put( key, value, nil )
+    }
+}
+
+
+func Set( key []byte, value []byte ) {
+    _db.Lock()
+    defer _db.Unlock()
+
+    SetUnsafe( key, value )
+}
+
+
+func GetUnsafe( key []byte ) []byte {
+    val, err := _db.ldb.Get( key, nil )
+
+    if err != nil {
+        return nil
+    }
+
+    return val
+}
+
+
+func Get( key []byte ) []byte {
+    _db.Lock()
+    defer _db.Unlock()
+
+    return GetUnsafe( key )
+}
+
+
+func Del( key []byte ) {
+    _db.Lock()
+    defer _db.Unlock()
+    DelUnsafe( key )
+}
+
+
+func DelUnsafe( key []byte ) {
+    _db.ldb.Delete(key, nil)
+}
+
+
+func Lock() {
+    _db.Lock()
+}
+
+
+func Unlock() {
+    _db.Unlock()
+}
+
+
+func TotalUnsafe( prefix []byte) int64 {
+
+    iter := _db.ldb.NewIterator(util.BytesPrefix(prefix), nil)
+    defer iter.Release()
+
+    i    := int64(0)
+
+    for iter.Next() {
+        i++
+    }
+
+    return i
+}
+
+
+func Total( prefix []byte ) int64 {
+    _db.Lock()
+    defer _db.Unlock()
+    return TotalUnsafe( prefix )
+}
+
+
+func ListUnsafe( prefix []byte, limit int, offset int, RemovePrefix bool ) [][]byte {
+
+    iter := _db.ldb.NewIterator(util.BytesPrefix(prefix), nil)
+    defer iter.Release()
+
+    res  := make( [][]byte, 0 )
+    i    := -1
+
+    for iter.Next() {
+        i++
+
+        if i >= offset + limit {
+            break
+        }
+
+        if i < offset {
+            continue
+        }
+
+        var list []byte
+
+        if( RemovePrefix ) {
+            size := len( iter.Key() ) - len(prefix)
+            list = make( []byte, size )
+            copy( list, ( iter.Key() )[ len(prefix) : ] )
+        } else {
+            list = make( []byte, len( iter.Key() ) )
+            copy( list, iter.Key() )
+        }
+
+        res = append( res, list )
+    }
+
+    return res
+}
+
+
+func List( prefix []byte, limit int, offset int, RemovePrefix bool ) [][]byte {
+    _db.Lock()
+    defer _db.Unlock()
+    return ListUnsafe( prefix, limit, offset, RemovePrefix )
+}
+
+
+func ForEachKey( prefix []byte, limit int, offset int, RemovePrefix bool, fn FOR_EACH_KEY_FUNC ) {
+    _db.Lock()
+    defer _db.Unlock()
+    ForEachKeyUnsafe( prefix, limit, offset, RemovePrefix, fn )
+}
+
+
+func ForEachKeyUnsafe( prefix []byte, limit int, offset int, RemovePrefix bool, fn FOR_EACH_KEY_FUNC ) {
+
+    iter := _db.ldb.NewIterator(util.BytesPrefix(prefix), nil)
+    defer iter.Release()
+
+    i    := -1
+
+    for iter.Next() {
+        i++
+
+        if i >= offset + limit {
+            break
+        }
+
+        if i < offset {
+            continue
+        }
+
+        var list []byte
+
+        if( RemovePrefix ) {
+            size := len( iter.Key() ) - len(prefix)
+            list = make( []byte, size )
+            copy( list, ( iter.Key() )[ len(prefix) : ] )
+        } else {
+            list = make( []byte, len( iter.Key() ) )
+            copy( list, iter.Key() )
+        }
+
+        if !fn(list) {
+            return
+        }
+    }
+}
+
---
+++ db/ldb/DB_test.go
@@ -0,0 +1,58 @@
+package db
+
+
+// @author  Mikhail Kirillov
+// @email   mikkirillov@yandex.ru
+// @version 1.000
+// @date    2017-05-18
+
+
+import (
+    "testing"
+)
+
+
+func TestDB( t *testing.T ) {
+    Init( "/tmp/helium.test" )
+    
+    key := []byte( "test.key" )
+    val := []byte{ 1, 2, 3 }
+
+    Del( key )
+
+    res := Get( key )
+
+    if res != nil {
+        t.Fatal( "expecting nil value" )
+    }
+
+    Set( key, val )
+
+    res = Get( key )
+
+    if res == nil || len(res) != len(val) {
+        t.Fatal( "db.Get error" )
+    }
+
+    for i, c := range res {
+        if c != val[i] {
+            t.Fatal( "db.Get error" )
+        }
+    }
+
+    Lock()
+    defer Unlock()
+
+    res = GetUnsafe( key )
+
+    if res == nil || len(res) != len(val) {
+        t.Fatal( "db.Get error" )
+    }
+
+    for i, c := range res {
+        if c != val[i] {
+            t.Fatal( "db.Get error" )
+        }
+    }
+}
+
