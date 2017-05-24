62d9524a741b80077707a34770b29181bfc5e716
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-05-24 08:12:17 +0300

    daemon

---
+++ daemon/Config.go
@@ -0,0 +1,15 @@
+package daemon
+
+
+// @author  Mikhail Kirillov
+// @email   mikkirillov@yandex.ru
+// @version 1.000
+// @date    2017-05-24
+
+
+type Config struct {
+    PidFile string `json:"pid"`
+    LogFile string `json:"log"`
+    WordDir string `json:"dir"`
+}
+
