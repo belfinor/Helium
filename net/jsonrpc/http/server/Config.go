1d94c34370ed0ad79e20eb1a908af06d5188483f
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-06-16 22:55:41 +0300

    jsonrcp server over http

---
+++ net/jsonrpc/http/server/Config.go
@@ -0,0 +1,14 @@
+package server
+
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2017-06-16
+
+
+type Config struct {
+    Host string `json:"host"`
+    Port int    `json:"port"`
+    Url  string `json:"url"`
+}
+
