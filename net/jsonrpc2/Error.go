9760820287f6adabcc5ddfb07a72e87bc7b15684
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-07-06 08:40:56 +0300

    add comments

--- net/jsonrpc2/Error.go
+++ net/jsonrpc2/Error.go
@@ -1,6 +1,11 @@
 package jsonrpc2
 
 
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2017-07-06
+
+
 type Error struct {
   Code int64 `json:"code"`
   Message string `json:"message"`
--- net/jsonrpc2/Handle.go
+++ net/jsonrpc2/Handle.go
@@ -1,6 +1,11 @@
 package jsonrpc2
 
 
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2017-07-06
+
+
 import (
     "encoding/json"
     "strings"
