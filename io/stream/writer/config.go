d7e92ae5150f92fc43a4b0e10ca87a8435670060
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-07-24 20:07:23 +0900

    update config

--- io/stream/writer/config.go
+++ io/stream/writer/config.go
@@ -6,6 +6,13 @@ package writer
 // @date    2017-07-24
 
 
+import(
+  "io/ioutil"
+  "strconv"
+  "strings"
+)
+
+
 type Config struct {
   Path   string `json:"path"`
   Index  string `json:"index"`
@@ -13,5 +20,24 @@ type Config struct {
   Save   int64  `json:"save"`
   Period int64  `json:"period"`
   LogId  int64  `json:"id"`
+}
+
+
+func (c *Config ) LoadLogId() {
+  if c.Buffer < 10 {
+    c.Buffer = 10
+  }
+
+  // load index file
+  if data, err := ioutil.ReadFile( c.Index ) ; err != nil {
+    c.LogId = 1
+    ioutil.WriteFile( c.Index, []byte("1"), 0664 )
+  } else {
+    str := strings.TrimSpace( string(data) )
+    if c.LogId, err = strconv.ParseInt( str, 10, 64 ) ; err != nil {
+      return
+    }
+    c.LogId++
+  }
 }
 
