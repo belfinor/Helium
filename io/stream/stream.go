a51da40a7c8c53cf6d695216b6c8650d9d97afe8
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-07-24 19:50:32 +0900

    stream reader

--- io/stream/stream.go
+++ io/stream/stream.go
@@ -9,12 +9,20 @@ package stream
 import (
   "bytes"
   "github.com/belfinor/Helium/pack"
+  "os"
 )
 
 
 type Stream struct {
     data []byte
     writer *Writer
+    file *os.File
+}
+
+
+
+type Reader interface {
+  OnData( []byte )
 }
 
 
@@ -25,6 +33,21 @@ func NewWriter( cfg *WriterConfig ) *Stream {
   }
 
   return st
+}
+
+
+func ReadStream( filename string, r Reader ) {
+  f, err := os.Open( filename )
+  if err != nil {
+    return
+  }
+
+  st := &Stream {
+    file: f,
+    data: make( []byte, 0, 4098 ),
+  }
+
+  st.read( r )
 }
 
 
@@ -50,6 +73,57 @@ func (s *Stream) Write( src []byte ) {
       list = list[size:]
     } else if len(list) == int(size) {
       s.writer.Push( list )
+      list = []byte{}
+    } else {
+      break;
+    }
+  }
+
+  if len(list) > 0 {
+    s.data = list
+  } else {
+    s.data = []byte{}
+  }
+}
+
+
+func (s *Stream) read(r Reader) {
+ 
+  buf := make( []byte, 4098 )
+ 
+  for {
+    n, e := s.file.Read(buf)
+
+    if e != nil {
+        break
+    }
+
+    if n < 4098 {
+      s.onRead( buf[0:n], r )
+      break;
+    } else {
+      s.onRead( buf, r )
+    }
+  }
+}
+
+
+func (s *Stream) onRead( data []byte, r Reader ) {
+  s.data = bytes.Join( [][]byte{  s.data, data }, []byte{} )
+  size := int16(0)
+
+  list := s.data
+
+  for len(list) > 2 {
+    if pack.Decode( list, &size ) != nil {
+      break
+    }
+    size = size + 2
+    if len(list) > int(size) {
+      r.OnData( list[2:size] )
+      list = list[size:]
+    } else if len(list) == int(size) {
+      r.OnData( list[2:] )
       list = []byte{}
     } else {
       break;
