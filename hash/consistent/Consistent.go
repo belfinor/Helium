2e28ff9552c9efd604205b28258b0c69eeabb3f6
Mikhail Kirillov <mikkirillov@yandex.ru>
2017-08-03 14:45:03 +0900

    consistent hashing

---
+++ hash/consistent/Consistent.go
@@ -0,0 +1,39 @@
+package consistent
+
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2017-07-28
+
+
+import (
+  "hash/crc32"
+)
+
+
+type Hash struct {
+  num   int
+  step  uint32
+}
+
+
+func New( nodes int ) *Hash {
+  h := &Hash{
+    num:  nodes,
+    step: uint32(0xffffffff) / uint32(nodes),
+  }
+
+  return h
+}
+
+
+func (h *Hash) Get( key []byte ) int {
+  v := crc32.ChecksumIEEE( key )
+  num := int(v / h.step)
+  if num >= h.num {
+    num = h.num - 1
+  }
+
+  return num
+}
+
