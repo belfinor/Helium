f996d6d4b2b38c4e401f3879085ab1c0f2f83acc
Mikhail Kirillov <mikkirillov@yandex.ru>
2018-03-15 09:02:28 +0300

    http client make agent string

--- net/http/client/client.go
+++ net/http/client/client.go
@@ -104,3 +104,17 @@ func (c *Client) Request( method string, url string, headers map[string]string, 
   return r, nil
 }
 
+
+func AgentString() string {
+    list := []string{
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36",
+        "Opera/9.80 (Windows NT 6.1; WOW64; U; ru) Presto/2.10.289 Version/12.00",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 YaBrowser/13.10.1500.9323 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.81 Chrome/58.0.3029.81 Safari/537.36",
+    }
+
+    return list[ int( time.Now().UnixNano() % int64( len(list) ) ) ]
+}
+
