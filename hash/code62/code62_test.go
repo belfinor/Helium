7a84fe88b9eb81b87ba9f992aee436e96633bef7
Mikhail Kirillov <mikkirillov@yandex.ru>
2018-04-26 12:33:14 +0300

    code62

---
+++ hash/code62/code62_test.go
@@ -0,0 +1,20 @@
+package code62
+
+
+// @author  Mikhail Kirillov <mikkirillov@yandex.ru>
+// @version 1.000
+// @date    2018-04-26
+
+
+import (
+  "testing"
+)
+
+
+func TestCalc( t *testing.T ) {
+
+  if Calc(205400) != "K0X" {
+    t.Fatal( "calc error" )
+  }
+}
+
