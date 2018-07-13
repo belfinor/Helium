f21030e558d1f5d7eb9b45b67c347bc73039a2a8
Mikhail Kirillov <mikkirillov@yandex.ru>
2018-07-13 08:40:02 +0300

    stemmer external stop words

--- text/stemmer/stemmer.go
+++ text/stemmer/stemmer.go
@@ -1,18 +1,31 @@
 package stemmer
 
 // @author  Mikhail Kirillov <mikkirillov@yandex.ru>
-// @version 1.001
-// @date    2018-07-10
+// @version 1.002
+// @date    2018-07-13
 
 import (
+	"bufio"
+	"os"
+
+	"github.com/belfinor/Helium/log"
 	"github.com/belfinor/Helium/text"
 	"github.com/belfinor/Helium/text/stemmer/english"
 	"github.com/belfinor/Helium/text/stemmer/russian"
+	"github.com/belfinor/Helium/text/token"
 )
+
+// Stop words
+//
+var stopWords map[string]string = map[string]string{}
 
 // Stem a word
 //
 func Word(word string) string {
+
+	if wf, has := stopWords[word]; has {
+		return wf
+	}
 
 	if text.IsRussian(word) {
 		return russian.Stem(word)
@@ -34,4 +47,46 @@ func Stream(input <-chan string) <-chan string {
 	}()
 
 	return output
+}
+
+// Load global stop words with fixed forms list
+//
+func LoadStopWords(filename string) {
+
+	fh, err := os.Open(filename)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer fh.Close()
+
+	log.Info("load stop words from file " + filename)
+
+	reader := bufio.NewReader(fh)
+	result := make(map[string]string)
+
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		list := token.Make(str)
+		if len(list) == 0 {
+			continue
+		}
+
+		if len(list) > 2 {
+			log.Error("error string: " + str)
+			continue
+		}
+
+		if len(list) == 1 {
+			list = append(list, list[0])
+		}
+
+		result[list[0]] = list[1]
+	}
+
+	stopWords = result
 }
