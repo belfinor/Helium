c56b54f1840a2a6102180b8ce1d3b4440565d4e4
Mikhail Kirillov <mikkirillov@yandex.ru>
2018-10-19 15:56:43 +0300

    up matrix

--- math/matrix/matrix.go
+++ math/matrix/matrix.go
@@ -23,6 +23,40 @@ func New(rows, cols int) *Matrix {
 	}
 }
 
+func Identity(size int) *Matrix {
+	m := New(size, size)
+
+	for i := 0; i < size; i++ {
+		m.data[i*size+i] = 1
+	}
+
+	return m
+}
+
+func FromSlice2(data [][]float64) *Matrix {
+	if data == nil {
+		return nil
+	}
+
+	rows := len(data)
+
+	if rows == 0 {
+		return nil
+	}
+
+	cols := len(data[0])
+
+	r := New(rows, cols)
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			r.data[i*cols+j] = data[i][j]
+		}
+	}
+
+	return r
+}
+
 func (m *Matrix) Clone() *Matrix {
 
 	r := &Matrix{
@@ -83,6 +117,16 @@ func Mul(m1, m2 *Matrix) *Matrix {
 	return r
 }
 
+func MulNum(m *Matrix, num float64) *Matrix {
+	r := New(m.rows, m.cols)
+
+	for i, v := range m.data {
+		r.data[i] = v * num
+	}
+
+	return r
+}
+
 func (m *Matrix) Trans() *Matrix {
 	r := New(m.cols, m.rows)
 
@@ -93,4 +137,77 @@ func (m *Matrix) Trans() *Matrix {
 	}
 
 	return r
+}
+
+func (m *Matrix) Slice2() [][]float64 {
+
+	res := make([][]float64, m.rows)
+
+	for i := 0; i < m.rows; i++ {
+
+		res[i] = make([]float64, m.cols)
+
+		for j := 0; j < m.cols; j++ {
+			res[i][j] = m.data[i*m.cols+j]
+		}
+	}
+
+	return res
+}
+
+func (m *Matrix) Get(i, j int) float64 {
+	return m.data[i*m.cols+j]
+}
+
+func (m *Matrix) Set(i, j int, v float64) {
+	m.data[i*m.cols+j] = v
+}
+
+func (m *Matrix) Row() int {
+	return m.rows
+}
+
+func (m *Matrix) Cols() int {
+	return m.cols
+}
+
+func (m *Matrix) Det() float64 {
+
+	r := m.Clone()
+
+	res := float64(1)
+
+	for i := 0; i < r.cols; i++ {
+		if r.data[i*r.cols+i] == 0 {
+			ok := false
+			for k := i + 1; k < r.rows; k++ {
+				if r.data[k*r.cols+i] != 0 {
+					ok = true
+					r.swapRows(i, k)
+					res *= -1
+					break
+				}
+			}
+			if !ok {
+				return 0
+			}
+		}
+
+		res *= r.data[i*r.cols+i]
+
+		for k := i + 1; k < r.rows; k++ {
+			v := r.data[k*r.cols+i] / r.data[i*r.cols+i]
+			for j := i; j < r.cols; j++ {
+				r.data[k*r.cols+j] -= v * r.data[i*r.cols+j]
+			}
+		}
+	}
+
+	return res
+}
+
+func (m *Matrix) swapRows(i1, i2 int) {
+	for j := 0; j < m.cols; j++ {
+		m.data[i1*m.cols+j], m.data[i2*m.cols+j] = m.data[i2*m.cols+j], m.data[i1*m.cols+j]
+	}
 }
