18b0374765c3d8e6d4fa90c3b2f51052d234cab7
Mikhail Kirillov <mikkirillov@yandex.ru>
2018-12-07 16:55:54 +0300

    text corpus default tags

--- text/corpus/tags/tags.go
+++ text/corpus/tags/tags.go
@@ -4,6 +4,11 @@ package tags
 // @version 1.001
 // @date    2018-12-07
 
+import (
+	"bufio"
+	"strings"
+)
+
 var fromCode map[uint16]string
 var toCode map[string]uint16
 
@@ -12,11 +17,78 @@ func init() {
 	fromCode = make(map[uint16]string, 128)
 	toCode = make(map[string]uint16, 128)
 
-	for i, v := range []string{"наука", "политика", "путешествия"} {
-		fromCode[uint16(i+1)] = v
-		toCode[v] = uint16(i + 1)
-	}
+	txt := `
+	18+
+	it
+	авиация
+	авто
+	армия
+	блогосфера
+	город
+	дача
+	дети
+	дизайн
+	еда
+	животные
+	здоровье
+	знаменитости
+	игры
+	искусство
+	история
+	кино
+	компьютеры
+	косметика
+	космос
+	криминал
+	литература
+	медицина
+	мода
+	музыка
+	наука
+	недвижимость
+	образование
+	общество
+	политика
+	природа
+	производство
+	происшествия
+	путешествия
+	работа
+	религия
+	ремонт
+	россия
+	семья
+	спорт
+	техника
+	технологии
+	фантастика
+	философия
+	финансы
+	эзотерика
+	экология
+	энергетика
+	`
+	br := bufio.NewReader(strings.NewReader(txt))
 
+	i := 0
+
+	for {
+
+		str, err := br.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		str = strings.TrimSpace(str)
+		if str == "" {
+			continue
+		}
+
+		fromCode[uint16(i+1)] = str
+		toCode[str] = uint16(i + 1)
+
+		i++
+	}
 }
 
 func ToCode(str string) uint16 {
